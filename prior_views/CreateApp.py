ea91d3b92e9e8fc7b0b06c98383af7ca0be1a465
Mark Morrison <markmorrison058@gmail.com>
2020-08-06 15:10:11 +0100

    trying to bypass initial param selection

--- prior_views/CreateApp.py
+++ prior_views/CreateApp.py
@@ -1,11 +1,19 @@
-from prior_views.Dashboards import PriorDashboard
+from prior_views.Dashboards import PriorDashboard, PosteriorDashboard, sample_trace_dashboard
+from tornado.ioloop import IOLoop
+import panel as pn
+import nest_asyncio
 
-def create_app():
-    prior = PriorDashboard(name='Prior_Dashboard')
-    prior_predictive = PriorPredictiveDashboard(name='Prior_Predictive_Dashboard')
-    posterior = PosteriorDashboard(name='Posterior_Dashboard')
-    posterior_predictive = posterior_predictive_Dashboard(name= 'posterior_predictive_dashboard')
-    sample_trace = sample_trace_dashboard(name='sample_trace_dashboard')
+def create_app(models={}):
+    prior_vars = list(list(models.values())[0].prior.data_vars)
+    posterior_vars = list(list(models.values())[0].posterior.data_vars)
+    prior = PriorDashboard(name='Prior_Dashboard',data=models)
+    prior.param.variable.objects = prior_vars
+    # prior_predictive = PriorPredictiveDashboard(name='Prior_Predictive_Dashboard')
+    posterior = PosteriorDashboard(name='Posterior_Dashboard',data=models)
+    posterior.param.variable.objects = posterior_vars
+    # posterior_predictive = posterior_predictive_Dashboard(name= 'posterior_predictive_dashboard')
+    sample_trace = sample_trace_dashboard(name='sample_trace_dashboard',data=models)
+    sample_trace.param.variable.objects = posterior_vars
     dashboard = pn.Tabs(
                     ('Prior',prior.panel().servable()),
                     # ('Prior_Predictive', prior_predictive.panel().servable()),
@@ -16,12 +24,8 @@ def create_app():
     dashboard
     return dashboard
 
-def create_prior_view(data, prior_views):
-    prior = PriorDashboard(data=data, name='Prior_Dashboard')
-    return prior.panel().servable()
-
-
-# posterior_predictive_inputs = list(pm_data.posterior_predictive.data_vars)
-# loop = IOLoop().current()
-# server = pn.serve(create_app, show=False, loop=loop, start=False, port=5006)
-# server.io_loop.start()
+def prior_checking_tool(models={}):
+    loop = IOLoop().current()
+    nest_asyncio.apply()
+    server = pn.serve(create_app(models), show=False, loop=loop, start=False, port=5006)
+    server.io_loop.start()
