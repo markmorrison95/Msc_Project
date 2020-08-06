8255163ea7f37f5faa746e3e921f0d4023f87988
Mark Morrison <markmorrison058@gmail.com>
2020-08-06 17:38:24 +0100

    now able to produce dashboard with one method call from jupyter is data is precollated

--- prior_views/CreateApp.py
+++ prior_views/CreateApp.py
@@ -3,29 +3,43 @@ from tornado.ioloop import IOLoop
 import panel as pn
 import nest_asyncio
 
+
 def create_app(models={}):
+    """
+    Pulls together the components required to create each tab
+    Initally pulls the variable names out of the data. In order to set the variable names for each dashboard
+    the dashboard needs to be initialised and then have the param for variable edited with the values. This is the
+    only way i could find to dynamically update with the variables as no way to pass to the class in a way that
+    param.Paramatized likes
+
+    All the components are then places into panel tabs and then the tabs object is returned
+    """
     prior_vars = list(list(models.values())[0].prior.data_vars)
     posterior_vars = list(list(models.values())[0].posterior.data_vars)
-    prior = PriorDashboard(name='Prior_Dashboard',data=models)
+    prior = PriorDashboard(name='Prior_Dashboard', data=models)
     prior.param.variable.objects = prior_vars
     # prior_predictive = PriorPredictiveDashboard(name='Prior_Predictive_Dashboard')
-    posterior = PosteriorDashboard(name='Posterior_Dashboard',data=models)
+    posterior = PosteriorDashboard(name='Posterior_Dashboard', data=models)
     posterior.param.variable.objects = posterior_vars
     # posterior_predictive = posterior_predictive_Dashboard(name= 'posterior_predictive_dashboard')
-    sample_trace = sample_trace_dashboard(name='sample_trace_dashboard',data=models)
+    sample_trace = sample_trace_dashboard(
+        name='sample_trace_dashboard', data=models)
     sample_trace.param.variable.objects = posterior_vars
     dashboard = pn.Tabs(
-                    ('Prior',prior.panel().servable()),
-                    # ('Prior_Predictive', prior_predictive.panel().servable()),
-                    ('Posterior', posterior.panel().servable()),
-                    # ('Posterior_Predictive', posterior_predictive.panel().servable()),
-                    ('Sample_Trace', sample_trace.panel().servable()),
-                    )
+        ('Prior', prior.panel().servable()),
+        # ('Prior_Predictive', prior_predictive.panel().servable()),
+        ('Posterior', posterior.panel().servable()),
+        # ('Posterior_Predictive', posterior_predictive.panel().servable()),
+        ('Sample_Trace', sample_trace.panel().servable()),
+    )
     dashboard
     return dashboard
 
+
 def prior_checking_tool(models={}):
     loop = IOLoop().current()
+    server = pn.serve(create_app(models), show=False, loop=loop, start=False)
+    # nest_asyncio required because if opening in jupyter notebooks, IOloop is already in use
     nest_asyncio.apply()
-    server = pn.serve(create_app(models), show=False, loop=loop, start=False, port=5006)
-    server.io_loop.start()
+    return server
+    # server.io_loop.start()
--- prior_views/Dashboards.py
+++ prior_views/Dashboards.py
@@ -7,65 +7,75 @@ from bokeh.plotting import curdoc
 from prior_views.ModelConversion import convert_models
 
 inputs_2 = ['Same Plot', 'Seperate Plots']
+
+"""
+Have to set the variables with default selectors containing none as there is no way to initialise the classes with 
+the variables already the. None allows for the plots to be called although will initially create with all the variables.
+Computationally this is not ideal, especially for sets with large data, but will just slow down initial load time and not
+slow the actual UI down.
+"""
 default_selectors = [None, None, None]
 
-    
+
 class PriorDashboard(param.Parameterized):
+    # precedence less than one means it is not a user displayed option. Set through initial params passed
     data = param.Dict(precedence=-1)
     variable = param.Selector(default_selectors)
     plot_type = param.Selector(inputs_2, default=inputs_2[0])
-    
+
     @param.depends('variable', 'plot_type', 'data')
     def plot(self):
         return prior_density_plot(variable=self.variable, data=self.data, plottype=self.plot_type)
-    
+
     def panel(self):
         return pn.Row(self.param, self.plot)
+
 
 class PriorPredictiveDashboard(param.Parameterized):
     data = param.Dict(precedence=-1)
     variable = param.Selector(default_selectors)
     plot_type = param.Selector(inputs_2, default=inputs_2[0])
-    
+
     @param.depends('variable', 'data')
     def plot(self):
         return prior_predictive_density_plot(variable=self.variable, data=self.data)
-    
+
     def panel(self):
         return pn.Row(self.param, self.plot)
-
 
 
 class PosteriorDashboard(param.Parameterized):
     data = param.Dict(precedence=-1)
     variable = param.Selector(default_selectors)
     plot_type = param.Selector(inputs_2, default=inputs_2[0])
-    
+
     @param.depends('variable', 'plot_type', 'data')
     def plot(self):
         return posterior_density_plot(variable=self.variable, data=self.data, plottype=self.plot_type)
-    
+
     def panel(self):
         return pn.Row(self.param, self.plot)
+
 
 class posterior_predictive_Dashboard(param.Parameterized):
     data = param.Dict(precedence=-1)
     variable = param.Selector(default_selectors)
-    
+
     @param.depends('variable', 'data')
     def plot(self):
         return posterior_predictive_density_plot(variable=self.variable, data=self.data)
-    
+
     def panel(self):
         return pn.Row(self.param, self.plot)
+
 
 class sample_trace_dashboard(param.Parameterized):
     data = param.Dict(precedence=-1)
     variable = param.Selector(default_selectors)
-    
+
     @param.depends('variable', 'data')
     def plot(self):
         return sample_trace_plot(variable=self.variable, data=self.data)
-    
+
     def panel(self):
         return pn.Row(self.param, self.plot)
