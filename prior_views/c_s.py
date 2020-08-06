6c048321bfb4275587383acac6a7017f143dd2b3
Mark Morrison <markmorrison058@gmail.com>
2020-08-06 17:39:13 +0100

    cleaned out some unused test files

--- prior_views/c_s.py
+++
@@ -1,6 +0,0 @@
-from prior_views.Dashboards import PriorDashboard
-
-def create_sometin(data, prior_views):
-    prior = PriorDashboard(name='Prior_Dashboard',data=data)
-    prior.variable.objects(prior_views)
-    return prior.panel().servable()
--- prior_views/test_final.py
+++
@@ -1,5 +0,0 @@
-from prior_views.Dashboards import PriorDashboard
-
-def please_work_please(data, prior_views):
-    prior = PriorDashboard(name='Prior_Dashboard',data=data)
-    return prior
