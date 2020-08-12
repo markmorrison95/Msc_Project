ec5a6240156d3642f514f9da25d272467433b21f
Mark Morrison <markmorrison058@gmail.com>
2020-08-12 09:36:58 +0100

    added methods for removing data in model conversion

--- prior_views/ModelConversion.py
+++ prior_views/ModelConversion.py
@@ -1,5 +1,8 @@
 import arviz as az
 import pymc3 as pm
+import random
+import numpy as np
+import pandas as pd
 
 def convert_models(models=[]):
     model_data = {}
@@ -18,3 +21,36 @@ def convert_models(models=[]):
             i+=1
             model_data.update({name :data})
     return model_data
+
+
+def data_reduce_with_nan(data, fraction):
+    """
+    Reduces data by selecting random rows in the data series and then replacing them with
+    numpy nan values. This is necessary when the number of data points is important, such
+    as when they represent constant intervals in time.
+
+    Throws type error if pandas dataframe of dataseries not used. 
+    """
+    if isinstance(data, pd.DataFrame) or isinstance(data, pd.Series):
+        size = len(data)
+        to_remove = int(size - (size*fraction))
+        data.loc[list(data.loc[random.sample(list(data.index), to_remove)].index)] = np.nan
+        return data
+    else:
+        raise TypeError("Only Pandas DataFrame or Series allowed")
+
+def reduce_data_remove(data, fraction):
+    """
+    Reduces data by selecting random rows in the DataFrame and removing them entirely.
+    Often replacing with nan values is not appropriate and will result in a model that 
+    cannot be sampled from properly 
+
+    Throws type error if pandas dataframe of dataseries not used. 
+    """
+    if isinstance(data, pd.DataFrame or isinstance(data, pd.Series)):
+        size = len(data)
+        to_remove = int(size - (size*fraction))
+        data.drop(list(data.loc[random.sample(list(data.index), to_remove)].index))
+        return data
+    else:
+        raise TypeError("Only Pandas DataFrame or Series allowed")
