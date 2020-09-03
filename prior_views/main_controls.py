df1759f4bc55c3bac0e5c911c61a4e5201134caa
Mark Morrison <markmorrison058@gmail.com>
2020-09-03 11:27:15 +0100

    catching errors when sampling from within GUI

--- prior_views/main_controls.py
+++ prior_views/main_controls.py
@@ -1,5 +1,6 @@
 from prior_views.models_container import model_container
 from prior_views.CreateApp import CreateApp
+from pymc3.exceptions import SamplingError
 
 
 class Main_controls:
@@ -10,8 +11,14 @@ class Main_controls:
 
 
     def add_new_model_config(self, prior_args:dict, name:str):
-        new_model = self.models.add_model(
-            prior_args=prior_args,
-            name = name,
-            )
-        self.app.new_model_added(new_prior_model=new_model)
+        try:
+            new_model = self.models.add_model(
+                prior_args=prior_args,
+                name = name,
+                )
+            self.app.new_model_added(new_prior_model=new_model)
+        except Exception:
+            # catches any exception from constructing and sampling the model
+            # not ideal. Would like to specify the error type and give a more informative
+            # warning message but struggling with catching some custom pymc3 exceptions
+            self.app.sampling_failed()
